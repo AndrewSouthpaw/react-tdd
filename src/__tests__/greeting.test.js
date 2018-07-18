49617dd21bb4ddb22f6a2a4e46b778d538c2fdf6
Andrew Smith <andrewsmith@alumni.stanford.edu>
2018-07-17 21:37:10 -0700

    Add some more stuff to Greeting for demo.

--- src/__tests__/greeting.test.js
+++ src/__tests__/greeting.test.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { Greeting } from '../greeting'
+import { sel, setupSandbox } from '../../shared/test_helpers'
+import { findByText, reflush } from '../../shared/test_helpers_react'
 
 describe('Greeting', () => {
+  setupSandbox()
+
+  const helloBtn = sel('dt-hello-btn')
+
   it('should work', () => {
     const w = mount(<Greeting />)
-    expect(w.text()).toEqual('Hello world')
+    helloBtn(w).simulate('click')
+  })
+
+  it('should provide the greeting', async () => {
+    const w = mount(<Greeting />)
+
+    await reflush(w)
+
+    expect(w.text()).toEqual('Hello worldGreet me')
+
+    findByText(/Greet/, w).simulate('click')
   })
 })
--- src/greeting.js
+++ src/greeting.js
@@ -1,3 +1,41 @@
 import React from 'react'
+import axios from 'axios'
 
-export const Greeting = () => (<p>Hello world</p>)
+export class MyButton extends React.Component {
+  render() {
+    return <button onClick={this.props.onClickButton}>{this.props.text}</button>
+  }
+}
+
+export class Greeting extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { greeting: '' }
+  }
+
+  async componentDidMount() {
+    // console.log('axios.get', axios.get)
+    // const res = await axios.get('/greeting').data
+    const res = await new Promise((resolve) => {
+      setImmediate(() => { resolve('Hello world') })
+    })
+    this.setState({ greeting: res })
+  }
+
+  render() {
+    const { greeting } = this.state
+    return (
+      <div>
+        <p>{greeting}</p>
+        <MyButton
+          onClickButton={() => {
+            console.log(greeting)
+          }}
+          data-test="dt-hello-btn"
+          text="Greet me"
+        />
+        <MyButton onClickButton={() => { console.log('goodbye') }} />
+      </div>
+    )
+  }
+}
